902c78f824f97d6805280c210eddf481e2a6c7eb
Dylan Atkins <dylanatkins30@gmail.com>
2026-08-10 01:48:10 -0400

    Load migrated data on deploy

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py loaddata data.json || true
 python manage.py createsuperuser --noinput --email admin@example.com || true
